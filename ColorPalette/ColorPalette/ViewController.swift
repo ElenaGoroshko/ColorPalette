f0364c4f79d065b5ae9f4378df77feea43311915
ElenaGoroshko <32428178+ElenaGoroshko@users.noreply.github.com>
2017-10-14 17:21:53 +0300

    2

--- ColorPalette/ColorPalette/ViewController.swift
+++ ColorPalette/ColorPalette/ViewController.swift
@@ -10,18 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelRed: UILabel!
+    @IBOutlet weak var labelGreen: UILabel!
+    @IBOutlet weak var labelBlue: UILabel!
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     @IBAction func buttonMedium(_ sender: Any) {
     }
-    
+
+    @IBAction func buttonMin(_ sender: Any) {
+    }
+
+    @IBAction func buttonMax(_ sender: Any) {
+    }
 }
 
